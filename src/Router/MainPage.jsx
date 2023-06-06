ee6525c97ee167b7582d5102390f268fec74fa3a
jeongwwon <spwldmlgla@naver.com>
2023-06-06 21:58:33 +0900

    commit

--- src/Router/MainPage.jsx
+++ src/Router/MainPage.jsx
@@ -99,10 +99,7 @@ function MainPage({ window }) {
     setMobileOpen(!mobileOpen);
   };
 
-  {
-    /*서버에 보낼 함수*/
-  }
-
+                 {/* 삭제할 함수 */}
   const handleUpload = (file) => {
     if (file.type.includes("image")) {
       const imageData = {
@@ -134,6 +131,7 @@ function MainPage({ window }) {
       setDocumentUrls((prevUrls) => [...prevUrls, documentData]);
     }
   };
+
 
   const handleDelete = () => {
     const selectedImages = Object.entries(childChecked)
@@ -183,85 +181,7 @@ function MainPage({ window }) {
     setChildChecked({});
   };
 
-  {
-    /*서버 호출 업로드*/
-  }
-  /*
-  const handleUpload = (file) => {
-    if (file.type.includes("image")) {
-      const imageData = new FormData();
-      imageData.append('dir','/Main');
-      imageData.append('file', file);
-      
-
-      fetch("http://43.207.224.148:8000/files/upload", {
-        method: "POST",
-        headers: {
-          "Content-Type": "multipart/form-data", 
-        },
-        body: imageData,
-      })
-        .then((response) => {
-          if (response.ok) {
-            console.log("response값:", response);
-            const newImageData = {
-              url: URL.createObjectURL(file),
-              fileName: file.name,
-              fileSize: file.size,
-              imgKey: `img-${Date.now()}`,
-            };
-            setImageUrls((prevUrls) => [...prevUrls, newImageData]);
-          } else {
-            throw new Error("Image upload failed");
-          }
-        })
-        .catch((error) => {
-          console.error("Error:", error);
-        });
-    } else if (
-      file.type.includes("application/pdf") ||
-      file.type.includes(".doc") ||
-      file.type.includes(".docx") ||
-      file.type.includes("application/msword") ||
-      file.type.includes("application/vnd.ms-excel") ||
-      file.type.includes(".xls") ||
-      file.type.includes(".xlsx") ||
-      file.type.includes(".csv") ||
-      file.type.includes(".ppt") ||
-      file.type.includes(".pptx") ||
-      file.type.includes("application/vnd.ms-powerpoint")
-    ) {
-      const documentData = new FormData();
-      documentData.append("dir", "/Main");
-      documentData.append("file", file.url);
-
-      fetch("http://43.207.224.148:8000/files/upload", {
-        method: "POST",
-        headers: {
-          "Content-Type": "multipart/form-data", 
-        },
-        body: documentData,
-      })
-        .then((response) => {
-          if (response.ok) {
-            console.log("response값:", response);
-            const newDocumentData = {
-              url: URL.createObjectURL(file),
-              fileName: file.name,
-              fileSize: file.size,
-              docKey: `doc-${Date.now()}`,
-            };
-            setDocumentUrls((prevUrls) => [...prevUrls, newDocumentData]);
-          } else {
-            throw new Error("Document upload failed");
-          }
-        })
-        .catch((error) => {
-          console.error("Error:", error);
-        });
-    }
-  };
-*/
+  
   const handleRestore = () => {
     const selectedImages = Object.entries(childChecked)
       .filter(([_, checked]) => checked.checked)
@@ -358,44 +278,9 @@ function MainPage({ window }) {
     setChildChecked({});
   };
 
-  {
-    /*서버 호출 업로드*/
-  }
-  /*
-  const handleUpload = (file) => {
-  const imageData = new FormData();
-  imageData.append('file', file);
-
-  fetch('http://43.207.224.148:8000/upload/file', {
-    method: 'POST',
-    body: imageData,
-  })
-    .then((response) => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        throw new Error('파일 업로드에 실패했습니다.');
-      }
-    })
-    .then((data) => {
-      const newImageData = {
-        url: data.url,
-        fileName: file.name,
-        fileSize: file.size,
-        imgKey: `img-${Date.now()}`,
-      };
-      setImageUrls((prevUrls) => [...prevUrls, newImageData]);
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-    });
-};
-*/
-
-  {
-    /*서버 파일 다운로드 */
-  }
-
+  
+          
+                              {/*삭제할 함수 */}
   const handleDownload = () => {
     const checkedKeys = Object.keys(childChecked).filter(
       (key) => childChecked[key].checked
